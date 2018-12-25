148e66264e89a2d23b5251d7ed4f31a5ef61b76b
xyf17469 <xyf17469@ly.com>
2018-12-25 14:20:33 +0800

    webpack优化

--- demo6/webpack.config.js
+++ demo6/webpack.config.js
@@ -2,24 +2,33 @@ const path = require('path');
 const webpack = require('webpack');
 
  // production development 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    module: {
-        rules: [
-            { 
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        modules: [path.resolve(__dirname, "src"), "node_modules"],
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-        publicPath: 'http://cdn.example.com/assets/'
-    }
+module.exports = function(env,argv){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve({
+                mode: 'development',
+                entry: {
+                    home: './src/home/index.js',
+                    list: './src/list/index.js'
+                },
+                module: {
+                    rules: [
+                        { 
+                            test: /\.js$/,
+                            use: 'babel-loader',
+                            exclude: /node_modules/
+                        }
+                    ]
+                },
+                resolve: {
+                    modules: [path.resolve(__dirname, "src"), "node_modules"],
+                },
+                output: {
+                    path: path.resolve(__dirname, 'dist'),
+                    filename: '[name]/bundle.js',
+                    publicPath: ''
+                }
+            })
+        }, 0)
+    })
 };
