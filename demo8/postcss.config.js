d7c7806126ebd5f05221d4987ca34e576e273e02
xyf <corner_scofield@163.com>
2019-04-30 14:24:23 +0800

    postcss转化rem去除node_modules

--- demo8/postcss.config.js
+++ demo8/postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     plugins: [
         require('autoprefixer')({ browsers : ['last 100 versions'] }),
-        require('postcss-plugin-px2rem')({ rootValue: 75, unitPrecision: 5,baseDpr: 2 })
+        require('postcss-plugin-px2rem')({ rootValue: 75, unitPrecision: 5,baseDpr: 2,exclude: /(node_module)/ })
     ]
 }
